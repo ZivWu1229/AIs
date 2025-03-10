2031ea64fd3c61f6336d3cd8ca4d5c759af0c5f4
ZivWu <zivwu2852@gmail.com>
2025-03-10 21:52:51 +0800

    Create get_stock.py

---
+++ stock/get_stock.py
@@ -0,0 +1,24 @@
+import requests
+import csv
+
+class Stock():
+    stock_num=0
+    def __init__(self,stock_num):
+        self.stock_num=stock_num
+    def getStock(self):
+        self.r = requests.post("https://www.cmoney.tw/api/internal/MobileService/ashx/GetDtnoData.ashx", data={"FilterNo": 0,"DtNo": "86489961","action": "getdtnodata","ParamStr":"AssignID=2330;CaptionMode=0;DTRange=1500;"})
+        print(self.r.ok)
+        print(self.r.json())
+    def make_csv(self,filename='stock.csv'):
+        with open(filename,'w',newline='') as file:
+            data=self.r.json()
+            writer=csv.writer(file)
+            title=['Date','Open','High','Low','Close','Volume']
+            writer.writerow(title)
+            writer.writerows(map(lambda x:x[0:len(title)],data['Data']))
+
+
+if __name__=="__main__":
+    stock=Stock(0)
+    stock.getStock()
+    stock.make_csv()
